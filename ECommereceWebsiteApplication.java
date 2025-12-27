3520d05c51c2d726aee09f6c8346ba4e999fb587
VIGNESHWARAN S <svignesh39305@gmail.com>
2025-12-27 19:02:01 +0530

    Add files via upload

---
+++ ECommereceWebsiteApplication.java
@@ -0,0 +1,11 @@
+package com.example.e_commerce.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommereceWebsiteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ECommereceWebsiteApplication.class, args);
+    }
+}
